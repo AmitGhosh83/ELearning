408e2036f035e1ef0d1f1bdea9bf9400cfcb2f1f
Amit Kumar Ghosh <Amit_kumar_Ghosh@hotmail.com>
2020-07-24 14:19:06 -0700

    Created Folder structure and implemented Autofac

---
+++ src/Elearning.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Elearning.Website.MvcApplication" Language="C#" %>
---
+++ src/Elearning.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Elearning.Website.Startup))]
+namespace Elearning.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
