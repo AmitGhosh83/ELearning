c674f7a225875a532edbc8b8f1266e1496a46894
Amit Kumar Ghosh <Amit_kumar_Ghosh@hotmail.com>
2020-07-24 14:45:07 -0700

    Updated SQL and Entity Framework

---
+++ src/Elearning.Database/DatabaseAccessor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elearning.Database
+{
+    public class DatabaseAccessor
+    {
+        private static readonly ProjectDBEntities entities;
+        
+        static DatabaseAccessor()
+        {
+            entities = new ProjectDBEntities();
+            entities.Database.Connection.Open();
+        }
+
+        public static ProjectDBEntities Instance
+        {
+            get
+            {
+                return entities;
+            }
+        }
+    }
+}
